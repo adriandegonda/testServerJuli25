b1d5b06392402c523fb17955e90ae48dce48bd0c
Adrian Degonda <adriandegonda@MacBook-Pro-von-Adrian.local>
2025-07-06 14:11:51 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const http = require('http');
+const WebSocket = require('ws');
+
+const port = process.env.PORT || 3000;
+
+const server = http.createServer();
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+
+  ws.on('message', (message) => {
+    console.log(`Received: ${message}`);
+    ws.send(`You said: ${message}`);
+  });
+
+  ws.on('close', () => {
+    console.log('Client disconnected');
+  });
+});
+
+server.listen(port, () => {
+  console.log(`WebSocket server running on port ${port}`);
+});
